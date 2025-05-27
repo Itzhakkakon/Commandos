6046dbd86eb2c2d28fa5063a347a24bbee954a59
Itzhak Kakon <izakkakon@gmail.com>
2025-05-27 10:37:30 +0300

    Remove content from Main method in Program class
    The Main method has been cleared of all previous functionality, including the creation and usage of Commando and Weapon objects. All related method calls and console output have been removed.

--- Commandos/Program.cs
+++ Commandos/Program.cs
@@ -3,14 +3,6 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Commando commando = new Commando("John", 123, "Rifle", "standing");
-            commando.Walk();
-            commando.Hide();
-            commando.Attack();
-            Weapon weapon = new Weapon("AK-47", "Kalashnikov", 30);
-            weapon.Shoot();
-            Console.WriteLine($"Weapon: {weapon.name}, Manufacturer: {weapon.manufacturer}, Remaining bullets: {weapon.numberOfBalls}");
-        }
+     
     }
 }
